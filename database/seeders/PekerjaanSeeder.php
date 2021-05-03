35046dc3d89052dfc7b81c1e4f690f3e51a23c68
riskiawp <riskianandawp@gmail.com>
2021-05-03 18:41:07 +0700

    done

--- database/seeders/PekerjaanSeeder.php
+++ database/seeders/PekerjaanSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Faker\Factory as Faker;
+use Illuminate\Support\Facades\DB;
 
 class PekerjaanSeeder extends Seeder
 {
@@ -13,6 +15,12 @@ class PekerjaanSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $faker = Faker::create('id_ID');
+        DB::table('pekerjaan')->insert([
+            'nama_pekerjaan' => $faker->jobName,
+            'gaji' => $faker->salary,
+            'created_at' =>  \Carbon\Carbon::yesterday(),
+    		'updated_at' => \Carbon\Carbon::now(),
+        ]);
     }
 }
